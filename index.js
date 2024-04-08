a015a7c7c0c0e59eeafa5ea7551f2736d0de9323
DenisaZem <xdenisa.zemanova@gmail.com>
2024-04-08 11:11:43 +0200

    Vypracováno

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('funguju!');
+const sec = Number(prompt("Zadej za kolik sekund minutka zazvoní:"))
+const SetTimer = sec*1000
+
+setTimeout(()=>{
+    const alarm = document.querySelector(".alarm")
+    alarm.classList.add("alarm--ring")
+}, SetTimer)
